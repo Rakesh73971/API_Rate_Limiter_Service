943e459ab8126e0a82bd4e3733dccddca62b2a50
Rakesh N <nraki522@gmail.com>
2026-03-08 15:38:05 +0530

    updated the main file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import auth,user
+from app.routes import auth,user,rate_limit_rule,request_log
 from . import models
 from .database import engine
 
@@ -10,3 +10,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(user.router)
+app.include_router(rate_limit_rule.router)
+app.include_router(request_log.router)

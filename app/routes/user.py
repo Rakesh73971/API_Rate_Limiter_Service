934a92f3933cc28bce488b5b94ebaec1dc25ab06
Rakesh N <nraki522@gmail.com>
2026-03-15 11:25:59 +0530

    fixed user profile api

--- app/routes/user.py
+++ app/routes/user.py
@@ -47,15 +47,17 @@ def create_user(user:UserBase,db:Session=Depends(get_db)):
     db.refresh(user)
     return user
 
-@router.get('/{id}',status_code=status.HTTP_200_OK,response_model=UserResponse)
-def get_user(id:int,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=UserResponse)
+def get_user(id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
     user = db.query(models.User).filter(models.User.id == id).first()
 
-    if user.id != current_user.id:
-        return HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail='Not allowed')
-
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='user not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='user not found')
+
+    
+    if user.id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Not allowed')
+
     return user
 
 @router.patch('/{id}',status_code=status.HTTP_202_ACCEPTED,response_model=UserResponse)
@@ -69,25 +71,3 @@ def update_user(id:int,user:UserUpdate,db:Session=Depends(get_db),current_user=D
     db_user.update(user.dict(exclude_unset=True),synchronize_session=False)
     db.commit()
     return db_user.first()
-
-@router.get("/profile")
-def get_profile(
-    db: Session = Depends(get_db),
-    current_user = Depends(get_current_user)
-):
-
-    rule = get_rate_limit_rule(db, current_user.role)
-
-    if not rule:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Rate limit rule not found"
-        )
-
-    check_rate_limit(
-        user_id=current_user.id,
-        limit=rule.requests_limit,
-        window=rule.time_window
-    )
-
-    return {"message": "User profile"}

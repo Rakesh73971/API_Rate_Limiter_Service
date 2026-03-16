6c39ede78ce8deb0a107d8ecd049bcb191062611
Rakesh N <nraki522@gmail.com>
2026-03-16 17:13:58 +0530

    tested the patch request

--- tests/test_user.py
+++ tests/test_user.py
@@ -14,3 +14,15 @@ def test_get_user(authorized_access, test_user):
     user_id = test_user['id']
     response = authorized_access.get(f"/users/{user_id}")
     assert response.status_code == 200
+
+
+def test_update_user(authorized_access, test_user):
+    updated_data = {
+        "full_name": "Rakesh Kumar"
+    }
+    response = authorized_access.patch(
+        f"/users/{test_user['id']}",
+        json=updated_data
+    )
+    assert response.status_code == 202
+    assert response.json()["full_name"] == "Rakesh Kumar"
